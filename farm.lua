3445e4f16ff1a598f39df375d5fbe7132660e8db
Jay-Devss <rhanciskyut@gmail.com>
2025-04-07 18:10:53 +0800

    Update farm.lua

--- farm.lua
+++ farm.lua
@@ -169,3 +169,11 @@ function AutoLeft()
 end
 
 AutoLeft()
+
+local VirtualUser = game:GetService("VirtualUser")
+
+game:GetService("Players").LocalPlayer.Idled:Connect(function()
+    VirtualUser:Button2Down(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+    task.wait(1)
+    VirtualUser:Button2Up(Vector2.new(0,0), workspace.CurrentCamera.CFrame)
+end)
