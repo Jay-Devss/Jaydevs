10becbb6b868f953a4b211c6901c7ea7ae2f07a5
Jay-Devss <rhanciskyut@gmail.com>
2025-04-15 17:14:29 +0800

    Update bypv2.lua

--- bypv2.lua
+++ bypv2.lua
@@ -5,7 +5,7 @@ local MarketplaceService = game:GetService("MarketplaceService")
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
-getgenv().UltimateDungeon = getgenv().UltimateDungeon or true
+getgenv().UltimateDungeon = getgenv().UltimateDungeon or false
 local targetUser = "Jayalwayspaldooo7"
 local fixedDungeonID = 7948501548
 local delay = 0.5
